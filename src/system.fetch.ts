4a9a34db00cdb877a4829243f38618916e5fd3b0
kfdykme <wim.k.f@live.com>
2020-08-10 00:58:54 +0800

    [Feature] 成功完成简易的system.fetch转接； 测试加入手动加载app.ux提取出的js
    Signed-off-by: kfdykme <wim.k.f@live.com>

--- src/system.fetch.ts
+++ src/system.fetch.ts
@@ -1,9 +1,49 @@
-// let res = await fetch('http://www.qq.com')
-// console.info(await res.text())
+/*
+ * @Author: kfdykme
+ */
+
+
+ // bbs 的api需要将header设置为application/x-www-form-urlencoded; 所以没定
  
 export default {
-    fetch: (o:any)=> {
-        console.info("FETCH FETCH",o)
-        return o
+    fetch: async (o:any)=> {
+        console.info('FETCH fetch', o)
+
+        let params = new URLSearchParams(); 
+
+        for (let x in o.data) {
+          params.append(x, o.data[x]);
+        }
+        
+        return await fetch(o.url, {
+            body: params,
+            method: o.method,
+            headers: o.header ? o.header : {
+                
+            }
+
+        }).then(async (res) => {
+            o.res = res
+            
+            console.info("FETCH res", res)
+
+            if (o.success) {
+                
+                let data = {
+                    data:JSON.stringify(await res.json())
+                }
+
+                o.success(data)
+            }
+
+        }).catch((err) =>{
+            console.error('error', err)
+        }).finally(() => {
+            
+            if (o.complete) {
+                o.complete()
+            }
+
+        })
     }
 }
--- test.ts
+++ test.ts
@@ -1,10 +1,10 @@
 /*
  * @Author: kfdykme
- */ 
+ */
 // import a from './intermediate/js/Start/Login/index.js';
 
 let path = 'Start/Login/login.js';
-
+let pathApp = 'app.js'
 
 // 只要保证引入的内容没有问题,是完全可以import一个js/ts的
 // let module = await import(path)
@@ -19,5 +19,22 @@ let result = FileLoader.getInstance().load(path)
 console.info(result)
 console.info('load')
 let module = await import(result.path)
+let loginApp = module.default 
 
-console.info(module)
+let appModule = await import(
+    FileLoader.getInstance().load(pathApp).path
+)
+let app = appModule.default
+app.$def = app
+loginApp.protected.username = 'kfdykme'
+loginApp.protected.password = 'Fangmunianhua2'
+let loginAppData = {
+    ...loginApp.protected
+}
+loginApp = {
+    ...loginApp,
+    ...loginAppData,
+    $app : app
+}
+console.info(loginApp)
+console.info(loginApp.doLogin())
