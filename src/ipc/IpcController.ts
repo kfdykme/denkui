31c890b5ceb9638149dd041a53e515b8852685ca
kfdykme <wim.k.f@live.com>
2022-03-26 16:09:49 +0800

    update deno

--- src/ipc/IpcController.ts
+++ src/ipc/IpcController.ts
@@ -1,17 +1,17 @@
-import { WebSocket, WebSocketServer } from "https://deno.land/x/websocket@v0.0.5/mod.ts";
+import { WebSocketClient, WebSocketServer } from "https://deno.land/x/websocket@v0.1.3/mod.ts";
 import logger from '@/log/console.ts'
 
 export default class IpcController {
     
     wss:WebSocketServer
-    ws:WebSocket|null = null
+    ws:WebSocketClient|null = null
     callbacks:Function[] = []
 
 
     constructor(port:number) {
         this.wss = new WebSocketServer(port)
         logger.info("IpcController init at port ", port)
-        this.wss.on("connection", (ws: WebSocket) => {
+        this.wss.on("connection", (ws: WebSocketClient) => {
             this.ws = ws
             logger.info('IpcController ws connection success at', new Date())
             this.ws.on("message", (message:any) => { 
